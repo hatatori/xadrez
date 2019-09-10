db55dbb81fe31b181bdc4bd8930b56378e37ff2d
Hatatori Nitsukawa <kcireh.i@gmail.com>
2019-09-10 00:11:46 -0300

    Update js.js

--- js.js
+++ js.js
@@ -40,56 +40,94 @@ for(i=0;i<8;i++){
 
 			nome_peca = renderPeca(org[num])
 			
+			color = (nome_peca.match(/2/g)) ? "black" : "white"
+			
 			div = document.createElement("div")
+		
 			div.id = val[(j)]+(9-(i+1))
+			div.y = i
 			div.style.transform = "translate("+j*50+"px,"+i*50+"px)"
 			div.setAttribute("class","p "+nome_peca)
-			
+			div.setAttribute("cor",color)
+		
 			tab[nome_coordenada].el = div
 			tabuleiro.appendChild(div)
 		}
 	}	
 }
 
+//verifica cor da peca da coordenada
+
+function verifica_cor(cor){
+	cor = document.getElementById(cor)
+	if(cor != null)
+		return cor.getAttribute("cor")
+	else
+		return false
+}
+
+//verifica de quem é a vez da jogada
+
+
+jogador = {
+	vez:'white'
+}
+
+//verifica se determinada peça existe na coordenada
+
+function verifica_casa(cord){
+	cord = document.getElementById(cord)
+	return (cord != null) ? true : false
+}
+
 //moveTo('a1','d4') // movimentação das peças
 
-function moveTo(cor_1,cor_2){
+function moveTo(cor_1,cor_2,color){
 	try{
-		
-		dx = tab[cor_2][0]
-		dy = tab[cor_2][1]
+		if(verifica_cor(cor_1) == color){
+			dx = tab[cor_2][0]
+			dy = tab[cor_2][1]
 
+			if(jogador.vez == 'white')
+					jogador.vez = 'black'
+				else
+					jogador.vez = 'white'
 
+			if(cor_1 != cor_2 && verifica_casa(cor_1)){
 
-		if(cor_1 != cor_2){
+				if(tab[cor_2].el != undefined  ){
+					comeu.play()
+				}else{
+					move.play()
+				}
 
-			if(tab[cor_2].el != undefined){
-				comeu.play()
-			}else{
-				move.play()
+				apagar = tab[cor_2].el
+				tab[cor_1].el.id = cor_2 
+				tab[cor_2].el = tab[cor_1].el
+				tab[cor_1].el.style.transform = 'translate('+dx+'px, '+dy+'px)'
+				tab[cor_1] = tab[cor_1].slice(0,2)
+				apagar.remove()
+
+				
 			}
-
-			apagar = tab[cor_2].el
-			tab[cor_1].el.id = cor_2 
-			tab[cor_2].el = tab[cor_1].el
-			tab[cor_1].el.style.transform = 'translate('+dx+'px, '+dy+'px)'
-			tab[cor_1] = tab[cor_1].slice(0,2)
-			apagar.remove()
 		}
 		
 	}catch(e){}
 }
 
-tabuleiro.onmousedown=function(e){
+tabuleiro2.onmousedown=function(e){
 	p1 = click
 }
 
-tabuleiro.onmouseup=function(e){
+tabuleiro2.onmouseup=function(e){
+
 	p2 = click
-	moveTo(p1,p2)
+	
+	moveTo(p1,p2,jogador.vez)
+	
 }
 
-tabuleiro.onmousemove=function(e){
+tabuleiro2.onmousemove=function(e){
 
 	x = e.layerX
 	y = e.layerY
